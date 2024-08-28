708ce4d1160ad1b65bd764c60a30c3951dd2ff03
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2024-08-28 12:21:01 -0700

    update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -7,3 +7,11 @@ X = np.arange(xmin, xmax, 0.1)
 ax.set_xlim([xmin, xmax])
 ax.set_ylim([-0.1, 1.1])
 m = -1
+
+for m in np.arange(0, 6, 0.1):
+    ax.plot(X, m * X )
+ax.scatter(0, 0, color="r")
+ax.scatter(0, 1, color="r")
+ax.scatter(1, 0, color="r")
+ax.scatter(1, 1, color="g")
+plt.plot()
