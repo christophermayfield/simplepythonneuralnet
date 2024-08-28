1894f1f678af2a3061a3ef99303f603271850076
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2024-08-28 12:20:36 -0700

    update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,2 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+fig, ax = plt.subplots()
+xmin, xmax = -0.2, 1.4
+X = np.arange(xmin, xmax, 0.1)
+ax.set_xlim([xmin, xmax])
+ax.set_ylim([-0.1, 1.1])
+m = -1
