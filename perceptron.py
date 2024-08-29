c7bc20e30f2d823aaea7fdc7a5f3d02b8fa8889b
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2024-08-29 11:30:20 -0700

    add perceptron class

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from perceptrons import Perceptron 
 import numpy as np
 
 fig, ax = plt.subplots()
@@ -15,3 +16,23 @@ ax.scatter(0, 1, color="r")
 ax.scatter(1, 0, color="r")
 ax.scatter(1, 1, color="g")
 plt.plot()
+
+
+
+
+def labelled_samples(n):
+    for _ in range(n):
+        s = np.random.randint(0, 2, (2,))
+        yield (s, 1) if s[0] == 1 and s[1] == 1 else (s, 0)
+
+p = Perceptron(weights=[0.3, 0.3, 0.3],
+               learning_rate=0.2)
+
+for in_data, label in labelled_samples(30):
+    p.adjust(label, 
+             in_data)
+
+test_data, test_labels = list(zip(*labelled_samples(30)))
+
+evaluation = p.evaluate(test_data, test_labels)
+print(evaluation)
