92f6d52ddaf596fd72ab2f1ea9f4da87508a1e75
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-01-20 21:42:31 -0800

    Update code

--- hello.py
+++ hello.py
@@ -5,3 +5,13 @@
 print('hi chris')
 stripped = "hello chris".strip()
 print(stripped)
+
+def print_name():
+    print("hello chris")
+
+num = 10
+def set_num():
+    num = 5
+    print(num)
+print(num)
+set_num()
