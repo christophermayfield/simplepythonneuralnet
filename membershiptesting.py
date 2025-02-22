670a582e985a81f22443e300c4e647132bef51fa
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-02-21 20:43:52 -0800

    update daily notes

--- membershiptesting.py
+++ membershiptesting.py
@@ -1,5 +1,5 @@
 #membership testing in python 
-#updating
+#updating --
 docs = 'Tuples are immutable sequences, typically used to store collections of heterogeneous data (such as the 2-tuples produced by the enumerate() built-in). Tuples are also used for cases where an immutable sequence of homogeneous data is needed (such as allowing storage in a set or dict instance).'
 """
 if 'tuple' not in docs:
