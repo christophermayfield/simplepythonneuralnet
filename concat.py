3d5cf351d776be2e73c8b8c3cd3b9036652441a6
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-02-26 10:46:48 -0800

    add concat

---
+++ concat.py
@@ -0,0 +1,11 @@
+object1 = [1,2,3,4,5]
+object2 = [6,7,8,9,10]
+
+#for mutable types like lists concatenation can be done in 
+#place the original list can be a larger list
+
+#for immutable datatypes like tuples and string, 
+# contacentation involes creating an entirely new object
+# not memory efficient so when making repeat concatenations
+#on an immutable type, consider a new data structure
+
