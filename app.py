15e5aecc2163316d3516daea9c09856346577a3d
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-03-01 20:15:40 -0800

    add myapp

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import dm
+
+print(__name__, "= app.py's value")
 print('hello from app.py')
+
+dm.gather_ingredients()
--- dm.py
+++ dm.py
@@ -1,1 +1,15 @@
-print("hello from dm.py")
+def gather_ingredients():
+    print('Bread, jelly, and peanut butter gathered.')
+    
+
+def combine_ingredients():
+    print('Add peanut butter and jelly to slices and put them together')
+
+def main():
+    gather_ingredients()
+    combine_ingredients()
+
+
+print(__name__," = dm.py's value")
+if __name__ == "__main__": 
+    main()
