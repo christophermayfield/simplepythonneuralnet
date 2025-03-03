73c718250a31c070996b22947051688a480d98a7
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-03-03 15:17:25 -0800

    add import

---
+++ alphabet.py
@@ -0,0 +1,11 @@
+
+
+def mysortedlist(list):
+    sortedlist = sorted(list)
+    first = sortedlist[0]
+    return first
+
+
+if(__name__ == "__main__"):
+    print('hello from alphabet.py')
+    print(mysortedlist(['chris', 'zebra', 'turtle', 'angelica']))
---
+++ myapp.py
@@ -0,0 +1,5 @@
+import alphabet
+word_list = ['hamster', 'turtle', 'cat', 'bird']
+last_names = ['rodriguez', 'lee', 'yakinen', 'amendola']
+
+print('the first animal is',alphabet.mysortedlist(word_list))
