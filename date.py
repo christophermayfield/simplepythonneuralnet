fc0d6cc0b6b1ca75a8a199243d5b52054fa01cd2
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-03-17 15:39:32 -0700

    add code to data.py

--- date.py
+++ date.py
@@ -1,5 +1,6 @@
 import time 
 import datetime
+import math
 #number of seconds passed since Jan 1 1970
 current_timestamp = time.time()
 
@@ -17,5 +18,41 @@ print(whatever)
 my_date = datetime.date(2025,3,15) #yesterday (it was currently 2025-3-16)
 
 print(my_date)
+
+#datetime 
+dt = datetime.datetime(2023,8,3,5)
+
+#current time 
+dt_now = datetime.datetime.now()
+print(dt)
+print(dt.month)
+print(dt_now)
+
+#combine brings together a date object and a time object 
+d = datetime.date.today()
+t = datetime.time(8)
+print(datetime.datetime.combine(d,t))
+
+#to extract a date from the datetime object we can use .date()
+w = datetime.datetime.now()
+print('the date is ', w.date())
+
+#to extract a time from the datetime object we can use .time()
+print('the current time is', w.time())
+
+#replace allows us to replace a particular property of the datetime object 
+print('changing the year to [1999] ->', w.replace(1999))
+
+
+
+#allows us to format a datetime with a string that we choose
+print(w.strftime('%b %d %-I %p')) #Mar 17 time am/pm
+print(w.strftime("%a %b %d @%-I %p"))
+
+
+#parse string - super useful when we have to parse user input
+print(w.strptime('27 June 2024','%d %B %Y'))
+print(w.strptime('16:00 16/08/2024','%H:%M %d/%m/%Y'))
+
 
 #calculating the difference between times within delta and time delta
