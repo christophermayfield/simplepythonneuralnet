d24d8414ba2a5103af556ac7f3a8e2cb27597242
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-05-30 11:31:08 -0700

    update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,2 +1,12 @@
+#lambda is a function that takes in two arguments and returns the sum of the two arguments
+
 add = lambda x,y: x + y
 print(add(5,3))
+
+
+#this is similar to the following function
+def add(x,y):
+    return x + y
+print(add(5,3))
+
+
